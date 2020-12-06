3830f8a3091618ee6a7bd28fedd3f115e15a8ddc
Truong Thi An Hai <55710763+TruongThiAnHai@users.noreply.github.com>
2020-12-06 14:23:13 +0300

    Delete Delete_review.sql

--- Delete_review.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER delete_review
-INSTEAD OF DELETE ON reviews_film                 
-
-BEGIN
-   DELETE from REVIEWS
-        WHERE review_id = :old.review_id OR movie_id = :old.movie_id;
-END;
-/
